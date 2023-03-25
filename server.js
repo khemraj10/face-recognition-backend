cad13611c1a041fcc7946189ee27b1ce6cc85811
Khemraj <khemrajg9806@gmail.com>
2023-03-25 17:51:46 +0530

    database connection modified

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ config();
 const db = knex({
 	client: 'pg',
 	connection: {
+		connectionString: process.env.DATABASE_URL,
+		ssl: { rejectUnauthorized: false },
 		host: process.env.DATABASE_LOCALHOST,
+		port: 5432,
 		user: process.env.DATABASE_USER,
 		password: process.env.DATABASE_PASSWORD,
 		database: process.env.DATABASE_NAME
